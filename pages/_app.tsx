76ea628548de5b7bd51bc3057cbd828f129cc395
Blake Wilson <blake@blake.id>
2023-01-20 15:47:36 -0600

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZBNHG6D2P2"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ZBNHG6D2P2');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
